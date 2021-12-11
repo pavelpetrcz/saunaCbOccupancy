a993501e4c3cf33adcec2aaa6793fca01b92ce41
pavelpetrcz <ppetr@ppetr.cz>
2021-12-11 11:19:19 +0100

    logging, changed logic

--- startApp.py
+++ startApp.py
@@ -1,8 +1,16 @@
-import scraper
-from time import sleep
+import actions
+import logging
 
 if __name__ == '__main__':
-    while True:
-        # run every minute
-        # sleep(60)
-        scraper.scrape()
+    logging.basicConfig(filename='saunaCbOccupancy.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s')
+    try:
+        actual_occupancy = 1
+        while True:
+            if actual_occupancy == 0:
+                wait = 20 if actions.isCloseToWholeHour() else wait = 300
+                actual_occupancy = actions.getValueFromPage()
+            else:
+                actual_occupancy = actions.scrape(60)
+    except BaseException as e:
+        logging.warning("main activity failed")
+        logging.exception(e)
